8997c5d319796dd46a5ae666b6ae9a6771ef5a33
alexjohnson514 <alexjohnson514@outlook.com>
2016-12-24 18:14:43 +0800

    Server Source Code

---
+++ src/main/resources/static/angular/call/callController.js
@@ -0,0 +1,95 @@
+/*
+ * @author Micael Gallego (micael.gallego@gmail.com)
+ * @author Raquel Díaz González
+ */
+
+kurento_room.controller('callController', function ($scope, $window, ServiceParticipant, ServiceRoom, Fullscreen, LxNotificationService) {
+
+    $scope.roomName = ServiceRoom.getRoomName();
+    $scope.userName = ServiceRoom.getUserName();
+    $scope.participants = ServiceParticipant.getParticipants();
+    $scope.kurento = ServiceRoom.getKurento();
+
+    $scope.leaveRoom = function () {
+
+        ServiceRoom.getKurento().close();
+
+        ServiceParticipant.removeParticipants();
+
+        //redirect to login
+        $window.location.href = '#/login';
+    };
+
+    window.onbeforeunload = function () {
+    	//not necessary if not connected
+    	if (ServiceParticipant.isConnected()) {
+    		ServiceRoom.getKurento().close();
+    	}
+    };
+
+
+    $scope.goFullscreen = function () {
+
+        if (Fullscreen.isEnabled())
+            Fullscreen.cancel();
+        else
+            Fullscreen.all();
+
+    };
+
+    $scope.onOffVolume = function () {
+        var localStream = ServiceRoom.getLocalStream();    
+        var element = document.getElementById("buttonVolume");
+        if (element.classList.contains("md-volume-off")) { //on
+            element.classList.remove("md-volume-off");
+            element.classList.add("md-volume-up");
+            localStream.audioEnabled = true;
+        } else { //off
+            element.classList.remove("md-volume-up");
+            element.classList.add("md-volume-off");
+            localStream.audioEnabled = false;
+
+        }
+    };
+
+    $scope.onOffVideocam = function () {
+        var localStream = ServiceRoom.getLocalStream();
+        var element = document.getElementById("buttonVideocam");
+        if (element.classList.contains("md-videocam-off")) {//on
+            element.classList.remove("md-videocam-off");
+            element.classList.add("md-videocam");
+            localStream.videoEnabled = true;
+        } else {//off
+            element.classList.remove("md-videocam");
+            element.classList.add("md-videocam-off");
+            localStream.videoEnabled = false;
+        }
+    };
+
+    //chat
+    $scope.message;
+
+    $scope.sendMessage = function () {
+        console.log("Sending message", $scope.message);
+        var kurento = ServiceRoom.getKurento();
+        kurento.sendMessage($scope.roomName, $scope.userName, $scope.message);
+        $scope.message = "";
+    };
+
+    //open or close chat when click in chat button
+    $scope.toggleChat = function () {
+        var selectedEffect = "slide";
+        // most effect types need no options passed by default
+        var options = {direction: "right"};
+        if ($("#effect").is(':visible')) {
+            $("#content").animate({width: '100%'}, 500);
+        } else {
+            $("#content").animate({width: '80%'}, 500);
+        }
+        // run the effect
+        $("#effect").toggle(selectedEffect, options, 500);
+    };
+    
+});
+
+
